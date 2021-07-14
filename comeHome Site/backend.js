7ae1c216d41a1fb84c019076f74fcfe340b5434c
RedMelody <an.red.amelody@gmail.com>
2021-07-13 20:18:55 -0500

    Add files via upload

---
+++ comeHome Site/backend.js
@@ -0,0 +1,20 @@
+// js file for both front and back
+
+/* 
+    Locations of shelters in an area
+    disctance between each shelter
+    nearest pet supplies in store
+    exoctic pet vets locations
+    donation to SPCA
+    links to other pet adoption sites
+    chatroom/ posting
+    post/upload pictures
+    video player and link to youtube
+*/
+
+
+
+
+
+
+
